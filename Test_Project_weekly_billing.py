3737cbfaa0e9539c504367612edc4658e77cb79f
Chethan-Rana <91674731+Chethan-Rana@users.noreply.github.com>
2021-10-16 09:33:17 +0530

    Updated the basic values

--- Test_Project_weekly_billing.py
+++ Test_Project_weekly_billing.py
@@ -8,13 +8,13 @@ class TestWeeklyBilling(unittest.TestCase):
         self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").total_hours_spent,167.95)
         #self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").total_hours_spent,167.95)
 
-    def test_projec(self):
-        self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").project_name,"19210_3M_CSS")    
-        self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").total_hours_spent,167.95)
+    #def test_projec(self):
+       # self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").project_name,"19210_3M_CSS")    
+        #self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").total_hours_spent,167.95)
 
-    def test_proje(self):
-        self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").project_name,"19210_3M_CSS")    
-        self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").total_hours_spent,167.95)       
+    #def test_proje(self):
+        #self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").project_name,"19210_3M_CSS")    
+        #self.assertEqual(Project_weekly_billing.Project("19210_3M_CSS").total_hours_spent,167.95)       
 
 
 if __name__ == "__main__":
